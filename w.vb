b5c5e23405566e208fec87b7bbb1f649346d6c64
AhmedShlak <167680165+AhmedShlak@users.noreply.github.com>
2024-04-28 10:18:11 +0200

    Update2 v11 w.vb

--- w.vb
+++ w.vb
@@ -1,6 +1,9 @@
-dim m as integer
 dim j as integer
 dim i as integer
-for i=1 to 10
- if i mod 2 =0 then print i
-next iw.vb
+for j = 1 to 20
+   for i=1 to 10
+     if i mod 2 =0 then print i
+      list1.addItem i
+   next i
+print j
+next j
